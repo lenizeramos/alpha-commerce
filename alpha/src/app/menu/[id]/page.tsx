776cbcd0db7082760c42ca2a5d2b5781bccb6781
Kevin Patino <kevinpatcastrejon95@gmail.com>
2025-02-09 12:11:39 -0800

    add link on menu page to show new details page

---
+++ alpha/src/app/menu/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailsPage = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default DetailsPage;
--- alpha/src/app/menu/page.tsx
+++ alpha/src/app/menu/page.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { RootState, AppDispatch } from "../context/store";
 import { fetchData } from "../context/slices/DataSlice";
+import Link from "next/link";
 
 const PrincessSofia = Princess_Sofia({
   weight: "400",
@@ -35,39 +36,40 @@ export default function Menu() {
             const imageUrl = image?.fields?.file.url || "";
 
             return (
-              <div
-                key={id}
-                className="containerItem flex flex-col items-center justify-center bg-white pb-8 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl"
-              >
-                <div className="containerImg w-full h-40 mb-4 relative overflow-hidden">
-                  <Image
-                    src={`https:${imageUrl}`}
-                    alt={name}
-                    //   width={200}
-                    //   height={100}
-                    fill
-                    className="rounded-lg"
-                  />
-                </div>
-                <div className="info w-full flex flex-col items-start px-8 gap-2">
-                  <div className="rating flex text-yellow-500">
-                    {Array.from({ length: rating }, (_, index) => (
-                      <span key={index}>★</span>
-                    ))}
+              <Link key={id} href={`/menu/${id}`}>
+                <div
+                  className="containerItem flex flex-col items-center justify-center bg-white pb-8 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl"
+                >
+                  <div className="containerImg w-full h-40 mb-4 relative overflow-hidden">
+                    <Image
+                      src={`https:${imageUrl}`}
+                      alt={name}
+                      //   width={200}
+                      //   height={100}
+                      fill
+                      className="rounded-lg"
+                    />
                   </div>
-                  <h2 className="text-xl font-semibold text-gray-800 text-center">
-                    {name}
-                  </h2>
-                  <div className="price flex justify-between w-full items-center">
-                    <p className="text-lg font-medium text-green-600 mb-2">
-                      ${price.toFixed(2)}
-                    </p>
-                    <div className="icon rounded-full bg-gray-200 text-black w-10 h-10 flex items-center justify-center cursor-pointer">
-                      <BsBasket2 size={20} className="cursor-pointer" />
+                  <div className="info w-full flex flex-col items-start px-8 gap-2">
+                    <div className="rating flex text-yellow-500">
+                      {Array.from({ length: rating }, (_, index) => (
+                        <span key={index}>★</span>
+                      ))}
+                    </div>
+                    <h2 className="text-xl font-semibold text-gray-800 text-center">
+                      {name}
+                    </h2>
+                    <div className="price flex justify-between w-full items-center">
+                      <p className="text-lg font-medium text-green-600 mb-2">
+                        ${price.toFixed(2)}
+                      </p>
+                      <div className="icon rounded-full bg-gray-200 text-black w-10 h-10 flex items-center justify-center cursor-pointer">
+                        <BsBasket2 size={20} className="cursor-pointer" />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             );
           })
         ) : (
