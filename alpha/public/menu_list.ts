3025a00f10ffa643f432dc25f9e8eb92d708d998
LilyFoNo <lilyfono21@gmail.com>
2025-02-10 20:50:35 -0800

    filters section

---
+++ alpha/public/menu_list.ts
@@ -0,0 +1,11 @@
+
+export const menu_list = [
+    {name:'Salad', img:'/filters/menu_1.png'},
+    {name:'Rolls', img:'/filters/menu_2.png'},
+    {name:'Deserts', img:'/filters/menu_3.png'},
+    {name:'Sandwich', img:'/filters/menu_4.png'},
+    {name:'Cake', img:'/filters/menu_5.png'},
+    {name:'Pure Veg', img:'/filters/menu_6.png'},
+    {name:'Pasta', img:'/filters/menu_7.png'},
+    {name:'Noodles', img:'/filters/menu_8.png'},
+]
--- alpha/src/app/menu/page.tsx
+++ alpha/src/app/menu/page.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import { BsBasket2 } from "react-icons/bs";
 import { Princess_Sofia } from "next/font/google";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react"; 
+import { useEffect, useState } from "react";
 import { RootState, AppDispatch } from "../context/store";
 import { fetchData } from "../context/slices/DataSlice";
 import Link from "next/link";
 import CartAnimation from "../components/CartAnimation";
 import { addToCart } from "../context/slices/CartSlice";
 import { CartItem } from "../types/SliceTypes";
+import Filters from "../components/Filters";
 
 const PrincessSofia = Princess_Sofia({
   weight: "400",
@@ -18,11 +19,10 @@ const PrincessSofia = Princess_Sofia({
 
 export default function Menu() {
   const dispatch: AppDispatch = useDispatch();
-  const { data, loading, error } = useSelector(
-    (state: RootState) => state.data
-  );
+  const { data } = useSelector((state: RootState) => state.data);
+  const [filter, setFilter] = useState<string>("All");
 
-  const [showAnimation, setShowAnimation] = useState(false);
+  const [showAnimation, setShowAnimation] = useState<boolean>(false);
 
   useEffect(() => {
     if (data.length === 0) {
@@ -40,65 +40,77 @@ export default function Menu() {
 
   return (
     <>
-      {showAnimation && <CartAnimation />} 
-      <div
-        className={`ContainerMenu grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-6xl mx-auto py-8 ${PrincessSofia.className}`}
-      >
-        {data.length > 0 ? (
-          data.map((entry: any) => {
-            const { id, name, image, price, description, category, rating } =
-              entry.fields as any;
-            const imageUrl = image?.fields?.file.url || "";
-
-            return (
-              <Link key={id} href={`/menu/${id}`}>
-                <div
-                  className="containerItem flex flex-col items-center justify-center bg-white pb-8 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl"
-                >
-                  <div className="containerImg w-full h-40 mb-4 relative overflow-hidden">
-                    <Image
-                      src={`https:${imageUrl}`}
-                      alt={name}
-                      fill
-                      className="rounded-lg"
-                    />
-                  </div>
-                  <div className="info w-full flex flex-col items-start px-8 gap-2">
-                    <div className="rating flex text-yellow-500">
-                      {Array.from({ length: rating }, (_, index) => (
-                        <span key={index}>★</span>
-                      ))}
+      {showAnimation && <CartAnimation />}
+      <div className="mainContainer flex flex-col items-center mt-7 mx-8">
+        <Filters category={filter} setCategory={setFilter} />
+        <div
+          className={`ContainerMenu grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-6xl mx-auto py-8 ${PrincessSofia.className}`}
+        >
+          {data.length > 0 ? (
+            data.map((entry: any) => {
+              const { id, name, image, price, description, category, rating } =
+                entry.fields as any;
+              const imageUrl = image?.fields?.file.url || "";
+              if (filter === "All" || category === filter) {
+                return (
+                  <Link key={id} href={`/menu/${id}`}>
+                    <div className="containerItem flex flex-col items-center justify-center bg-white pb-8 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl">
+                      <div className="containerImg w-full h-40 mb-4 relative overflow-hidden">
+                        <Image
+                          src={`https:${imageUrl}`}
+                          alt={name}
+                          fill
+                          sizes="auto"
+                          className="rounded-lg"
+                        />
+                      </div>
+                      <div className="info w-full flex flex-col items-start px-8 gap-2">
+                        <h2 className="text-xl font-semibold text-gray-800 text-center">
+                          {name}
+                        </h2>
+                        <div className="rating flex">
+                          {Array.from({ length: 5 }, (_, index) => (
+                            <span
+                              key={index}
+                              className={
+                                index < rating
+                                  ? "text-yellow-500"
+                                  : "text-gray-400"
+                              }
+                            >
+                              ★
+                            </span>
+                          ))}
+                        </div>
+                        <div className="price flex justify-between w-full items-center">
+                          <p className="text-lg text-purple-900 mb-2 font-bold">
+                            ${price.toFixed(2)}
+                          </p>
+                          <button
+                            onClick={(e) => {
+                              e.preventDefault();
+                              handleAddToCart({
+                                name,
+                                image: imageUrl,
+                                price,
+                                rating,
+                              });
+                            }}
+                            className="icon rounded-full bg-gray-200 text-black w-10 h-10 flex items-center justify-center cursor-pointer"
+                          >
+                            <BsBasket2 size={20} />
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                    <h2 className="text-xl font-semibold text-gray-800 text-center">
-                      {name}
-                    </h2>
-                    <div className="price flex justify-between w-full items-center">
-                      <p className="text-lg font-medium text-green-600 mb-2">
-                        ${price.toFixed(2)}
-                      </p>
-                      <button
-                        onClick={(e) => {
-                          e.preventDefault();
-                          handleAddToCart({
-                            name,
-                            image: imageUrl,
-                            price,
-                            rating,
-                          });
-                        }}
-                        className="icon rounded-full bg-gray-200 text-black w-10 h-10 flex items-center justify-center cursor-pointer"
-                      >
-                        <BsBasket2 size={20} />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            );
-          })
-        ) : (
-          <div className="text-center">No items available</div>
-        )}
+                  </Link>
+                );
+              }
+            })
+          ) : (
+            <div className="text-center ">No items available</div>
+          )}
+        </div>
       </div>
     </>
   );
