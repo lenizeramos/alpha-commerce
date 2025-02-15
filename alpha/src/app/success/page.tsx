bb312a33bf165004c70143d78cc9fc1552dc8597
LilyFoNo <lilyfono21@gmail.com>
2025-02-14 19:52:37 -0800

    duration of animation for mobile and tablet screens

--- alpha/src/app/success/page.tsx
+++ alpha/src/app/success/page.tsx
@@ -1,7 +1,10 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { Tomorrow } from "next/font/google";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../context/slices/CartSlice";
 
 const textFont = Tomorrow({
   weight: "400",
@@ -10,10 +13,16 @@ const textFont = Tomorrow({
 });
 
 export default function checkout() {
+  const dispatch = useDispatch()
+  useEffect (()=>{
+    dispatch(clearCart())
+  },[])
   return (
     <>
-      <div className={`${textFont.className} flex flex-col xl:gap-60 sm:gap-20 gap:10 xl:px-36 p-10 relative`}>
-      <div className="absolute -left-5 md:top-5">
+      <div
+        className={`${textFont.className} flex flex-col xl:gap-60 sm:gap-20 gap:10 xl:px-36 p-10 relative`}
+      >
+        <div className="absolute -left-5 md:top-5">
           <Image
             src={"/decoration/img12.png"}
             alt=""
@@ -21,7 +30,7 @@ export default function checkout() {
             height={300}
           />
         </div>
-      <div className="hidden md:block absolute right-5 bottom-0 opacity-15">
+        <div className="hidden md:block absolute right-5 bottom-0 opacity-15">
           <Image
             src={"/decoration/img11.png"}
             alt=""
@@ -29,14 +38,20 @@ export default function checkout() {
             height={400}
           />
         </div>
-      <motion.h1
-      initial={{ opacity: 0, y: -20, scale: 0.8 }}
-      animate={{ opacity: 1, y: 0,scale: 1 }}
-      transition={{ duration: 0.8, ease: "easeOut", type: "spring", stiffness: 100 }}
-      className="text-checkout md:text-7xl sm:text-5xl text-3xl text-orange-800 text-center sm:pt-14"
-    >
-      Thanks a ton! Your order is on its way. Can&apos;t wait for you to receive it!
-    </motion.h1>
+        <motion.h1
+          initial={{ opacity: 0, y: -20, scale: 0.8 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            ease: "easeOut",
+            type: "spring",
+            stiffness: 100,
+          }}
+          className="text-checkout md:text-7xl sm:text-5xl text-3xl text-orange-800 text-center sm:pt-14"
+        >
+          Thanks a ton! Your order is on its way. Can&apos;t wait for you to
+          receive it!
+        </motion.h1>
         <div className="relative">
           <Image
             src={"/delivery.png"}
